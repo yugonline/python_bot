3cd4aba1a21068919531b4fef47709f7404f11c3
yugonline <yugakaeminem@gmail.com>
2017-06-05 23:54:57 -0400

    Add Yo Command

--- bot.py
+++ bot.py
@@ -7,10 +7,15 @@ def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater('YOUR TOKEN HERE')
+def yo(bot,update):
+	update.message.reply_text(
+		'Yo! {}'.format(update.message.from_user.first_name))
+
+updater = Updater('340664104:AAG3RpwP4maWNbO2FtTCu2xqvGGFLU_F2Xs')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('yo',yo))
 
 updater.start_polling()
 updater.idle()
