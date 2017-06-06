2b8b3d2d12e0333d17e9a3f7723ab63ef4de8ee2
yugonline <yugakaeminem@gmail.com>
2017-06-06 00:39:09 -0400

    Still work with photos

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def start(bot, update):
     update.message.reply_text('Hello World!')
@@ -16,13 +16,20 @@ def photo(bot, update):
     newFile = bot.getFile(file_id)
     newFile.download('test.jpg')
     bot.sendMessage(chat_id=update.message.chat_id, text="download succesfull")
+def get_input(bot,update):
+	user = update.message.from_user
+	if update.message.photo:
+		update.message.reply_text("Thinking hard...")
+		photo_id = update.message.photo[-1].file_id
+		photo_file = bot.getFile(photo_id)
+		photo_file.download()
 
 updater = Updater('340664104:AAG3RpwP4maWNbO2FtTCu2xqvGGFLU_F2Xs')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('yo',yo))
-updater.dispatcher.add_handler(CommandHandler('photo',photo))
+updater.dispatcher.add_handler(MessageHandler(get_input))
 
 updater.start_polling()
 updater.idle()
